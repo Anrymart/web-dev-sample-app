81fb17352fd9dcc3bcfe1e2f064a832f9ae784b9
Anrymart <a.mart@list.ru>
2021-07-29 08:42:25 +0300

    healthcheck

--- backend/src/main/java/com/anrymart/wd3/backend/HealthController.java
+++ backend/src/main/java/com/anrymart/wd3/backend/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/api/health")
+    @GetMapping("/")
     public String health() {
         return "UP";
     }
